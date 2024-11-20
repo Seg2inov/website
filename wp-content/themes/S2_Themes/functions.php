768160c9b64eb6c5cedd9bb31a033231bc3d4038
Seg2inov <heloise@seg2inov.fr>
2024-11-20 16:45:51 +0100

    reimport css

--- wp-content/themes/S2_Themes/functions.php
+++ wp-content/themes/S2_Themes/functions.php
@@ -1,7 +1,10 @@
 <?php
+
 function s2_theme_enqueue_styles() {
+    // Enqueue reset.css
+    wp_enqueue_style('reset-css', get_template_directory_uri() . '/css/reset.css');
+
+    // Enqueue style.css
     wp_enqueue_style('s2-theme-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 's2_theme_enqueue_styles');
-
-
