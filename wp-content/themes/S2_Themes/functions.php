ca183588f0f67231e20c821c32aab0e200869276
Seg2inov <heloise@seg2inov.fr>
2024-11-20 16:57:05 +0100

    organise css

--- wp-content/themes/S2_Themes/functions.php
+++ wp-content/themes/S2_Themes/functions.php
@@ -1,10 +1,11 @@
 <?php
 
 function s2_theme_enqueue_styles() {
-    // Enqueue reset.css
     wp_enqueue_style('reset-css', get_template_directory_uri() . '/css/reset.css');
-
-    // Enqueue style.css
+    
+    wp_enqueue_style('blackButton-css', get_template_directory_uri() . '/css/blackButton.css');
+    wp_enqueue_style('header-css', get_template_directory_uri() . '/css/header.css');
+    
     wp_enqueue_style('s2-theme-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 's2_theme_enqueue_styles');
