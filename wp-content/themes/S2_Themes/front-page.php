194a5e5a1a61012eeb73e42afd7817dc15dcd2b8
Seg2inov <heloise@seg2inov.fr>
2024-11-22 14:37:10 +0100

    add metadonnée

--- wp-content/themes/S2_Themes/front-page.php
+++ wp-content/themes/S2_Themes/front-page.php
@@ -5,6 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seg2inov</title>
+    <meta name="description" content="Seg2Inov propose des logiciels de caisse innovants, spécialement conçus pour le commerce alimentaire et les systèmes de pesée. Simplifiez votre gestion et boostez votre efficacité avec nos solutions adaptées.">
+    <meta name="keywords" content="seg2inov, seg2 inov, seg 2 inov, logiciel de caisse, caisse, alimentaire, commerce alimentaire, logiciel de caisse pour épicerie, pesée, balances, solutions de gestion, gestion, ia, ai, intelligence artificiel, logiciel de caisse innovant, innovant, innovation, produit, client, fournisseurs, fournisseur, client, analyse, ticket, caisse, adaptable, fiable">
+    <meta name="robots" content="index, follow">
+
+
+    <meta property="og:title" content="Seg2Inov">
+    <meta property="og:description" content="Découvrez nos solutions de caisse et de pesée adaptées aux commerces alimentaires. Seg2Inov simplifie votre gestion avec des outils innovants et accessibles.">
+    <meta property="og:image" content="wp-content\themes\S2_Themes\assets\img\partout.png">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Seg2Inov">
+    <meta name="twitter:description" content="Simplifiez la gestion de votre commerce alimentaire avec nos logiciels de caisse innovants et nos solutions de pesée.">
+    <meta name="twitter:image" content="wp-content\themes\S2_Themes\assets\img\partout.png">
+    <meta property="og:url" content="https://www.seg2inov.eu">
+    <meta property="og:type" content="website">
+
+    <link rel="canonical" href="https://www.seg2inov.eu">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="wp-content\themes\S2_Themes\style.css">
 </head>
 <body>
