f0e781d6d40d3aa40674402b0aecc2e168e41d82
Seg2inov <heloise@seg2inov.fr>
2024-12-13 13:56:49 +0100

    test work

--- wp-content/themes/S2_Themes/front-page.php
+++ wp-content/themes/S2_Themes/front-page.php
@@ -275,6 +275,37 @@
     </div>
  </section>
 
+ <div class="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+    </div>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-scrollbar"></div>
+  </div>
+
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+  <script>
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  </script>
+
  <!-- ____________________________________________ilsnousfontConfiance__________________________________________________________________________ -->
  <section class="confiance">
 
