2333af465de94707083ec178451e6258ba75ef84
Seg2inov <heloise@seg2inov.fr>
2024-12-16 14:25:55 +0100

    add contact page

---
+++ wp-content/themes/S2_Themes/contact.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Template Name: Contact
+ * Template Post Type: page
+ */
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Seg2inov - Contact</title>
+        <meta name="robots" content="index, follow">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
+    </head>
+
+    <body>
+
+    <header>
+        <a href="/"><img class="logo"src="<?php echo get_stylesheet_directory_uri(); ?>\assets\logoseg2inov.png" alt="Seg2inov le commerce innovant"></a>
+        <div class="">
+            <!-- Se connecter -->
+             <a href="mailto:contact@seg2inov.com" class="blackButton"><p>Discutons de votre projet</p> <img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\topright.svg" alt="Clique"></a>
+        </div>
+    </header>
+    <div class="blurDiv"></div>
+
+
+
+        <section class="talk">
+            <div class="title">
+                <h2>
+                    Discutons de votre 
+                </h2>
+                <h2 class="part2Title">- projet ensemble    </h2>
+            </div>
+
+            <a href="mailto:contact@seg2inov.com">
+                <div class="big whiteButton">
+                    <p>contact@seg2inov.fr</p>
+                    <div class="partClique">
+                        <p>Envoyer un mail</p>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\right.svg" alt="clique mail">
+                    </div>
+                </div>
+            </a>
+            <a href="tel:+0970702696">
+                <div class="big whiteButton">
+                    <p>09 70 70 36 96</p>
+                    <div class="partClique">
+                        <p>Appeler</p>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\right.svg" alt="clique tel">
+                    </div>
+                </div>
+            </a>
+
+        </section>
+
+        <section class="reseau">
+            <div class="title">
+                <h2>
+                    Nos réseaux
+                </h2>
+                <h2 class="part2Title">- Sociaux   </h2>
+            </div>
+
+
+            <a href="https://www.linkedin.com/company/seg2inov/?viewAsMember=true">
+                <div class="small whiteButton">
+                    <p>Linkedin</p>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\right.svg" alt="clique mail">
+                </div>
+            </a>
+            <a href="https://www.instagram.com/seg2inov/">
+                <div class="samll whiteButton">
+                    <p>Instagram</p>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\right.svg" alt="clique tel">
+                </div>
+            </a>
+            <a href="https://www.facebook.com/people/Seg2inov/61562257696981/">
+                <div class="samll whiteButton">
+                    <p>Facebook</p>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\right.svg" alt="clique tel">
+                </div>
+            </a>
+
+
+        </section>
+        <div class="contenaireBackgroundFooter">
+ <footer>
+     <div class="contenaireFooter">
+         <h2>Collaborons ensemble pour une meilleure Optimisation</h2>
+         
+         <div class="">
+                <h3>Contact</h3>
+                <a href="mailto:contact@seg2inov.fr"><p>contact@seg2inov.fr</p></a>
+                <a href="mailto:support@seg2inov.fr"><p>support@seg2inov.fr</p></a>
+                <a href="tel:+0970702696"><p>09 70 70 26 96</p></a>
+            </div>
+            <div class="">
+                <h3>Où nous trouver ?</h3>
+                <a  class="voirPlusDetailButton" href="https://www.linkedin.com/company/seg2inov/?viewAsMember=true"><p>Linkedin</p><img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\toprightlight.svg" alt="clique"></a>
+                <a  class="voirPlusDetailButton" href="https://www.instagram.com/seg2inov/"><p>Instagram</p><img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\toprightlight.svg" alt="clique"></a>
+                <a  class="voirPlusDetailButton" href="https://www.facebook.com/people/Seg2inov/61562257696981/"><p>Facebook</p><img src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\toprightlight.svg" alt="clique"></a> 
+            </div>
+        </div>
+    <div class="linkunderfooter">
+    <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>\assets\logoseg2inov.png" alt="Seg2inov">
+    <p class="mention">Tous droits réservés à SEG2INOV  - <a href="/<?php echo get_stylesheet_directory_uri(); ?>/mentions-legales/"><p>Mentions légales</p></a> </p>
+     
+    </div>
+    </footer>
+    <img class="backgroundImg"src="<?php echo get_stylesheet_directory_uri(); ?>\assets\img\backgroudfooter.png" alt="fond">
+</div>
+
+    </body>
+
+</html>
