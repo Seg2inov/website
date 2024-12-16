de0e7ce522e5c20cb61260e44f05fd852739946f
Seg2inov <heloise@seg2inov.fr>
2024-12-16 11:51:02 +0100

    try add style, header and footer to mention

--- wp-content/themes/S2_Themes/mention.php
+++ wp-content/themes/S2_Themes/mention.php
@@ -10,64 +10,175 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Seg2inov - Mention lègales</title>
-        <meta name="description" content="Seg2Inov propose des logiciels de caisse innovants, spécialement conçus pour le commerce alimentaire et les systèmes de pesée. Simplifiez votre gestion et boostez votre efficacité avec nos solutions adaptées.">
-        <meta name="keywords" content="seg2inov, seg2 inov, seg 2 inov, logiciel de caisse, caisse, alimentaire, commerce alimentaire, logiciel de caisse pour épicerie, pesée, balances, solutions de gestion, gestion, ia, ai, intelligence artificiel, logiciel de caisse innovant, innovant, innovation, produit, client, fournisseurs, fournisseur, client, analyse, ticket, caisse, adaptable, fiable">
         <meta name="robots" content="index, follow">
-
-
-        <meta property="og:title" content="Seg2Inov">
-        <meta property="og:description" content="Découvrez nos solutions de caisse et de pesée adaptées aux commerces alimentaires. Seg2Inov simplifie votre gestion avec des outils innovants et accessibles.">
-        <meta property="og:image" content="wp-content\themes\S2_Themes\assets\img\partout.webp">
-
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="Seg2Inov">
-        <meta name="twitter:description" content="Simplifiez la gestion de votre commerce alimentaire avec nos logiciels de caisse innovants et nos solutions de pesée.">
-        <meta name="twitter:image" content="wp-content\themes\S2_Themes\assets\img\partout.webp">
-        <meta property="og:url" content="https://www.seg2inov.eu">
-        <meta property="og:type" content="website">
-
-        <link rel="canonical" href="https://www.seg2inov.eu">
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <link rel="stylesheet" href="wp-content\themes\S2_Themes\style.css">
     </head>
-    <h1>Mention lègales</h1>
 
-<p>
-Adresse : 9 Rue André Darbon 33 300, 33300 Bordeaux<br>
-Adresse e-mail : contact@seg2inov.fr<br>
-Numéro de téléphone : +33 9 70 70 26 96<br>
-Hébergement : o2switch<br><br>
+    <body>
 
-Conception<br>
-
-Création : SEG2 INOV<br>
-Crédits visuels : SEG2 INOV<br>
-
-Conditions Générales d’Utilisation (CGU)<br><br>
-
-L’accès à ce web signifie l’acceptation des présentes CGU.<br>
-En utilisant notre site, l’utilisateur s’engage à respecter les conditions générales d’utilisation du site suivantes. <br>
-
-Propriété intellectuelle <br><br>
-
-Tous les éléments présents sur le site (photographie, logo, illustration, son, design, article) sont protégés par la loi en vigueur au titre de la propriété intellectuelle. <br>
-En effet toute reproduction ou utilisation est strictement interdite sans accord au préalable de l'éditeur. <br>
-La reproduction et la copie des contenus par l'utilisateur requièrent une autorisation préalable du site. Dans ce cas, toute utilisation à des usages commerciaux ou à des fins publicitaires est proscrite. <br>
-
-Champ d’application / contenu utilisateur <br><br>
-
-Description des services : <br>
-
-SEG2 INOV est un éditeur et intégrateur de la solution d’encaissement S2CASH. <br>
-SEG2 INOV propose aux commerçants des outils digitaux leur permettant une meilleure gestion et suivi des encaissements. L’ERP 360 S2CASH, est un concentré d’outils issues de la grande distribution au service des commerçants, leur permettant d’offrir une expérience d’achat unique à leurs clients. <br>
-<br><br>
-Pour des raisons de maintenance ou autres, l’accès au site peut être interrompu ou suspendu par l’éditeur sans préavis ni justification.
-
-</p>
+    <header>
+        <a href="/"><img class="logo"src="wp-content\themes\S2_Themes\assets\logoseg2inov.png" alt="Seg2inov le commerce innovant"></a>
+        <div class="">
+            <!-- Se connecter -->
+             <a href="mailto:contact@seg2inov.com" class="blackButton"><p>Discutons de votre projet</p> <img src="wp-content\themes\S2_Themes\assets\arrow\topright.svg" alt="Clique"></a>
+        </div>
+    </header>
+    <div class="blurDiv"></div>
 
 
 
+        <article>
+            <div class="title">
+                <h2>
+                    Mention
+                </h2>
+                <h2 class="part2Title">- lègales  </h2>
+            </div>
+
+            <p>
+            Adresse : 9 Rue André Darbon 33 300, 33300 Bordeaux<br>
+            Adresse e-mail : contact@seg2inov.fr<br>
+            Numéro de téléphone : +33 9 70 70 26 96<br>
+            Hébergement : o2switch<br><br>
+
+            Conception<br>
+
+            Création : SEG2 INOV<br>
+            Crédits visuels : SEG2 INOV<br>
+
+            Conditions Générales d’Utilisation (CGU)<br><br>
+
+            L’accès à ce web signifie l’acceptation des présentes CGU.<br>
+            En utilisant notre site, l’utilisateur s’engage à respecter les conditions générales d’utilisation du site suivantes. <br>
+
+            Propriété intellectuelle <br><br>
+
+            Tous les éléments présents sur le site (photographie, logo, illustration, son, design, article) sont protégés par la loi en vigueur au titre de la propriété intellectuelle. <br>
+            En effet toute reproduction ou utilisation est strictement interdite sans accord au préalable de l'éditeur. <br>
+            La reproduction et la copie des contenus par l'utilisateur requièrent une autorisation préalable du site. Dans ce cas, toute utilisation à des usages commerciaux ou à des fins publicitaires est proscrite. <br>
+
+            Champ d’application / contenu utilisateur <br><br>
+
+            Description des services : <br>
+
+            SEG2 INOV est un éditeur et intégrateur de la solution d’encaissement S2CASH. <br>
+            SEG2 INOV propose aux commerçants des outils digitaux leur permettant une meilleure gestion et suivi des encaissements. L’ERP 360 S2CASH, est un concentré d’outils issues de la grande distribution au service des commerçants, leur permettant d’offrir une expérience d’achat unique à leurs clients. <br>
+            <br><br>
+            Pour des raisons de maintenance ou autres, l’accès au site peut être interrompu ou suspendu par l’éditeur sans préavis ni justification.
+
+            </p>
+
+            <img src="wp-content\themes\S2_Themes\assets\logoseg2inov.png" alt="Seg2 Inov">
+        </article>
+        <div class="contenaireBackgroundFooter">
+ <footer>
+     <div class="contenaireFooter">
+         <h2>Collaborons ensemble pour une meilleure Optimisation</h2>
+         
+         <div class="">
+                <h3>Contact</h3>
+                <a href="mailto:contact@seg2inov.fr"><p>contact@seg2inov.fr</p></a>
+                <a href="mailto:support@seg2inov.fr"><p>support@seg2inov.fr</p></a>
+                <a href="tel:+0970702696"><p>09 70 70 26 96</p></a>
+            </div>
+            <div class="">
+                <h3>Où nous trouver ?</h3>
+                <a  class="voirPlusDetailButton" href="https://www.linkedin.com/company/seg2inov/?viewAsMember=true"><p>Linkedin</p><img src="wp-content\themes\S2_Themes\assets\arrow\toprightlight.svg" alt="clique"></a>
+                <a  class="voirPlusDetailButton" href="https://www.instagram.com/seg2inov/"><p>Instagram</p><img src="wp-content\themes\S2_Themes\assets\arrow\toprightlight.svg" alt="clique"></a>
+                <a  class="voirPlusDetailButton" href="https://www.facebook.com/people/Seg2inov/61562257696981/"><p>Facebook</p><img src="wp-content\themes\S2_Themes\assets\arrow\toprightlight.svg" alt="clique"></a> 
+            </div>
+        </div>
+    <div class="linkunderfooter">
+    <img class="logo" src="wp-content\themes\S2_Themes\assets\logoseg2inov.png" alt="Seg2inov">
+    <p class="mention">Tous droits réservés à SEG2INOV</p>
+    <!--  - <a href="/wp-content\themes\S2_Themes\mention.php"><p>Mentions légales</p></a> -->
+    </div>
+    </footer>
+    <img class="backgroundImg"src="wp-content\themes\S2_Themes\assets\img\backgroudfooter.png" alt="fond">
+</div>
+
+    </body>
 
 </html>
+
+
+<style>
+
+@import url('../S2_Themes/css/footer.css');
+@import url('../S2_Themes/css/header.css');
+
+
+
+html, body {
+    background-color: #F5F5F7;
+}
+ 
+@font-face {
+    font-family: 'SofiaSans-Medium';
+    src: url('assets/font/SofiaSans-Medium.ttf');
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'SofiaSans-Light';
+    src: url('assets/font/SofiaSans-Light.ttf');
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'SofiaSans-Regular';
+    src: url('assets/font/SofiaSans-Regular.ttf');
+    font-display: swap;
+}
+
+
+h1{
+    font-family: 'SofiaSans-Regular';
+    font-size: 36px;
+}
+
+h2{
+    font-family: 'SofiaSans-Regular';
+    font-size: 36px;
+}
+@media screen and (max-width: 405px) {
+    h2{
+        font-size:26px;
+    }
+}
+
+
+p, h5, label, a, details, summary{
+    font-family: 'SofiaSans-Light';
+    font-size: 22px;
+}
+
+article{
+    width: 100%;
+    margin-left: 8vw;
+    margin-right: 8vw;
+}
+
+
+
+.title{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 101px;
+}
+
+
+
+
+
+.part2Title{
+    transform: translateX(106%);
+}
+@media screen and (max-width: 520px) {
+    .part2Title{
+        transform: translateX(0%);
+    }
+}
+
+</style>
