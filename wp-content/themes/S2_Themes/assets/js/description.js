9cd8ce3f5e9d9abb7e15582e48583018575ef83b
Seg2inov <heloise@seg2inov.fr>
2025-03-05 11:01:25 +0100

    try know change zone

--- wp-content/themes/S2_Themes/assets/js/description.js
+++ wp-content/themes/S2_Themes/assets/js/description.js
@@ -2,21 +2,13 @@ alert("Script chargé !");
 
 let description = document.querySelector('.description');
 let stickyElement = document.querySelector('.stickyElement');  // L'image à gauche
-
-console.log('description', description);
-
-// Variable pour éviter les multiples appels à `scroll`
 let isScrolling = false;
 
-// Fonction pour gérer l'événement de scroll
 const handleScroll = () => {
     if (!isScrolling) {
         window.requestAnimationFrame(() => {
-            console.log('isScrolling');
-            // Logique pour changer l'image selon la position de scroll
             let scrollPosition = window.scrollY || window.pageYOffset;
-
-            // Exemple de logique pour changer l'image en fonction du défilement
+            console.log('scrollPosition', scrollPosition)
             if (scrollPosition > 1000) {
                 console.log('change img')
                 //stickyElement.src = 'path_to_new_image.webp';  // Change l'image à un certain point du scroll
@@ -24,13 +16,9 @@ const handleScroll = () => {
                 console.log('img')
                 //stickyElement.src = 'path_to_original_image.webp';  // Remet l'image initiale
             }
-
             isScrolling = false;
         });
     }
-
     isScrolling = true;
 };
-
-// Ajouter l'écouteur d'événement de scroll
 document.addEventListener('scroll', handleScroll);
