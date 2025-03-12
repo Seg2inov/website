8fa648d8a2b2dca7d3030d75d07b0cf9f79e78cc
Seg2inov <heloise@seg2inov.fr>
2025-03-12 17:10:06 +0100

    build site

--- wp-content/themes/S2_Themes/docs/docusaurus.config.js
+++ wp-content/themes/S2_Themes/docs/docusaurus.config.js
@@ -15,17 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://seg2inov.eu',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
