58d10c15bc0fb2953fb616398c1485dadc434025
Seg2inov <heloise@seg2inov.fr>
2025-04-22 11:44:44 +0200

    Preuve d’engagement et pas certificat

--- wp-content/themes/S2_Themes/front-page.php
+++ wp-content/themes/S2_Themes/front-page.php
@@ -50,21 +50,21 @@
 <div class="containerWhite">
 <img class="afnor"src="<?php echo get_stylesheet_directory_uri(); ?>\assets\logo\afnor.PNG" alt="AFNOR">
     <div class="part1">
-        <h2>Certification AFNOR en cours  </h2>
+        <h2>Preuve d’engagement AFNOR en cours  </h2>
         <div class="links">
             
-        <p class="descriptifAfnor"><span class="orange bold">SEG2 INOV</span>  est inscrit dans un processus de certification <span class="bold">NF525</span> et <span class="bold">NF203</span> pour le logiciel <span class="orange bold">S2CASH</span>. <br> Certification enregistrée sous le numéro : FR 37 978 843 951 <br> Vous pouvez vérifier à tout instant la validité de cette preuve d’engagement en scannant  le QR-code ci-joint.</p>
+        <p class="descriptifAfnor"><span class="orange bold">SEG2 INOV</span>  est inscrit dans un processus de preuve d’engagement <span class="bold">NF525</span> et <span class="bold">NF203</span> pour le logiciel <span class="orange bold">S2CASH</span>. <br> Preuve d’engagement enregistrée sous le numéro : FR 37 978 843 951 <br> Vous pouvez vérifier à tout instant la validité de cette preuve d’engagement en scannant  le QR-code ci-joint.</p>
             <div class="linkDownload displayRow">
                 <a href="<?php echo get_stylesheet_directory_uri(); ?>\assets\Preuves_engagement.pdf">
                     <div class="BackgroundDownload">
                         <img class="downloadlogo"src="<?php echo get_stylesheet_directory_uri(); ?>\assets\arrow\download.svg" alt="Telecharger">
                     </div>
-                    <p>Télécharger le certificat </p>
+                    <p>Télécharger la preuve d’engagement </p>
                 </a>
             </div>
             <div class="QRcode displayRow">
                 <img class="qrcodelogo"src="<?php echo get_stylesheet_directory_uri(); ?>\assets\img\QRCode.png" alt="QRCode">
-                <p>Vérification de la validité du certificat </p>
+                <p>Vérification de la validité de la preuve d’engagement </p>
             </div>
         </div>
     </div>
